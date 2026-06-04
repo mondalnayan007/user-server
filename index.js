7c4c708ecab818d9cd98522d71d62491d6b5af77
Nayan Mondal <n007kingkong@gmail.com>
2026-06-04 23:27:05 +0600

    created the api to get single data

--- index.js
+++ index.js
@@ -160,6 +160,13 @@ async function run() {
          res.send(result);
     })
 
+    app.get('/users/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.findOne(query);
+      res.send(result)
+    })
+
 
 
 
